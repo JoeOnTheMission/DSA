d3103434e2f11956d5091b142dec0f624578333e
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-11 22:01:18 +0300

    Time: 498 ms (5.52%) | Memory: 18.2 MB (48.82%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,18 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-            n = 0
-            while n < len(heights):
-                for i in range(len(heights)-1):
-                    x = heights[i]
-                    y = heights[i+1]
-                    if x < y:
-                        temp = heights[i]
-                        heights[i] = heights[i+1]
-                        heights[i+1] = temp
+            min_index = 0
+            while min_index < len(heights):
+                for i in range(min_index + 1,len(heights)):
+                    if heights[min_index] < heights[i]:
+                        temp = heights[min_index]
+                        heights[min_index] = heights[i]
+                        heights[i ] = temp
 
-                        temp = names[i]
-                        names [i] = names[i+1]
-                        names[i+1] = temp
+                        temp = names[min_index]
+                        names[min_index] = names[i]
+                        names[i] = temp
 
-                n = n + 1
+                min_index = min_index + 1
             return names
