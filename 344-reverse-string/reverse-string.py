747e0719bb1d66c4c00b78b9cbbe531ecf3e1eb9
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-14 21:16:12 +0300

    Time: 0 ms (100.00%) | Memory: 23 MB (42.01%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,12 +3,4 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i = 0
-        j = -1
-        for i in range(len(s)//2):
-            temp = s[i]
-            s[i] = s[j]
-            s[j] = temp
-            j = j - 1
-        return s
-        
+        s.reverse()
