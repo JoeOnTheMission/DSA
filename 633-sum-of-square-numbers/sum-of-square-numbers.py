ac2fb13b4df00b79c273a61a9da8c67dd72d3ffd
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-18 19:32:29 +0300

    Time: 107 ms (18.99%) | Memory: 17.9 MB (31.91%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l = 0
+        r = c
+        while l <= r:
+            if (r**2)  > c:
+                r = r//2
+            elif(r**2)  <= c:
+                r = r*2
+                break
+        while l <= r:
+            if (r**2) + (l**2) > c:
+                r -= 1
+            elif(r**2) + (l**2) < c:
+                l += 1
+            else:
+                return True
+        return False 
