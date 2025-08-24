58324ea91ab2947a10b48d72b3ffccc35619f605
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-24 22:30:26 +0300

    Time: 83 ms (77.30%) | Memory: 23.1 MB (58.32%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -3,17 +3,17 @@ class Solution:
         l = 0
         res = 0
         can_start = 0
+        odd = 0
         for r in range(len(nums)):
-
-            if nums[r] % 2 != 0:#odd
-                k -= 1
-                can_start = 0
-
-            while k == 0:
-                if nums[l] % 2 != 0:#odd
-                    k += 1
+            if nums[r] % 2: #num is odd
+                odd += 1
+            while odd > k:
+                if nums[l] % 2:
+                    odd -= 1
                 l += 1
-
-                can_start += 1
-            res += can_start
+                can_start = l
+            if odd == k:
+                while not nums[can_start] % 2: #every even point from here makes a valid subarray until r
+                    can_start += 1
+                res += (can_start - l) + 1
         return res
