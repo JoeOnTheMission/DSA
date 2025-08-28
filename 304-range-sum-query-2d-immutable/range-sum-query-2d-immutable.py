d5b8f60dabb6c20843223fb616e99da81883d041
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-29 02:36:53 +0300

    Time: 76 ms (98.12%) | Memory: 31.2 MB (30.96%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,24 +1,22 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.matrix = matrix
-        self.point = [1,2,2,4]
         self.pre = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-        self.last_row = [0]*len(matrix[0])
+        last_row = [0]*len(matrix[0])
         for i in range(len(matrix)):
-            self.last = 0
+            last = 0
             for j in range(len(matrix[0])):
-                self.last += self.matrix[i][j]
-                self.last_row[j] += self.last
-                self.pre[i][j] = self.last_row[j]
+                last += matrix[i][j]
+                last_row[j] += last
+                self.pre[i][j] = last_row[j]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        self.a = self.pre[row2][col2]
-        self.b = self.pre[row1 - 1][col2] if row1 != 0 else 0
-        self.c = self.pre[row2][col1 - 1] if col1 != 0 else 0
-        self.d = self.pre[row1-1][col1-1] if col1 != 0 and row1 != 0 else 0
-        self.res = self.a - self.b - self.c + self.d
-        return self.res
+        a = self.pre[row2][col2]
+        b = self.pre[row1 - 1][col2] if row1 != 0 else 0
+        c = self.pre[row2][col1 - 1] if col1 != 0 else 0
+        d = self.pre[row1-1][col1-1] if col1 != 0 and row1 != 0 else 0
+        return a - b - c + d
+        
         
 
 
