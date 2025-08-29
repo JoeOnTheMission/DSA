2f05544245ba228ec168156ed4aa2b45b00977d5
JoeOnTheMission <joeonthemission@gmail.com>
2025-08-29 22:10:12 +0300

    Time: 44 ms (13.35%) | Memory: 22.2 MB (5.06%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        from collections import defaultdict
+
+        pre = defaultdict(int)
+        pre[0] = 1
+        res = 0
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            dif = total - k
+            res += pre[dif]
+            pre[total] +=1
+        return res
